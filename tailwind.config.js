387c417ef9c31e50349962ba1042f38ae9ab6a66
aaronhpchan <aaronhpchan@gmail.com>
2022-06-04 10:05:16 -0400

    Set up Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './views/**/*.ejs'
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
