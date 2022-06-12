48cf0d890f2993b6d3e585cc73f49f0dae524b1e
aaronhpchan <aaronhpchan@gmail.com>
2022-06-12 11:26:09 -0400

    Add Joi

---
+++ schemas.js
@@ -0,0 +1,10 @@
+const Joi = require('joi');
+
+module.exports.spotSchema = Joi.object({
+    spot: Joi.object({
+        title: Joi.string().required(),
+        location: Joi.string().required(),
+        image: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
